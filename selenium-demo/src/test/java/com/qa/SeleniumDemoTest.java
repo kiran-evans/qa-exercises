fc4d1ecfdf448aa76be1dfaa678b5082acc2a02f
kiran-evans <thekiranevans@gmail.com>
2022-08-15 11:36:12 +0100

    KE: Added await

--- selenium-demo/src/test/java/com/qa/SeleniumDemoTest.java
+++ selenium-demo/src/test/java/com/qa/SeleniumDemoTest.java
@@ -2,22 +2,27 @@ package com.qa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SeleniumDemo {
+public class SeleniumDemoTest {
 	
 	private RemoteWebDriver driver;
 	
 	@BeforeEach
 	void setup() {
-		this.driver = new ChromeDriver();
+		this.driver = new EdgeDriver();
+		this.driver.manage().window().maximize();
 	}
 	
 	@Test
@@ -28,7 +33,8 @@ public class SeleniumDemo {
 		search.sendKeys("dress");
 		search.sendKeys(Keys.ENTER);
 		
-		WebElement result = this.driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > h5 > a"));
+		WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(10));
+		WebElement result = (WebElement) wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > h5 > a")));
 		
 		assertEquals("Printed Summer Dress", result.getText());
 	}
