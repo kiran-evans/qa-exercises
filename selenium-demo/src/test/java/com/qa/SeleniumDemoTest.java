fc28129bb8df35546f255bc91a73994067210236
kiran-evans <thekiranevans@gmail.com>
2022-08-15 15:36:27 +0100

    KE: Added cart and checkout tests

--- selenium-demo/src/test/java/com/qa/SeleniumDemoTest.java
+++ selenium-demo/src/test/java/com/qa/SeleniumDemoTest.java
@@ -35,43 +35,48 @@ public class SeleniumDemoTest {
 		search.sendKeys("dress");
 		search.sendKeys(Keys.ENTER);
 		
-		WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(10)); // Wait for page to load
-		WebElement result = (WebElement) wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > h5 > a"))); // Select first item label
+		WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(20)); // Wait for page to load
+		WebElement result = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > h5 > a"))); // Select first item label
 		
 		assertEquals("Printed Summer Dress", result.getText());
 		
+		// Open product page
+		
+		WebElement goToProduct = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > h5 > a"))); // Find product
+		goToProduct.click();
+		
 		// Add to cart
 		
-		WebElement addToCart = this.driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(1) > div > div.right-block > div.button-container > a.button.ajax_add_to_cart_button.btn.btn-default")); // Find Add to cart 'a' tag
+		WebElement addToCart = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#add_to_cart > button"))); // Find Add to cart button
 		addToCart.click();
 		
 		// Proceed to checkout
 		
-		WebElement proceedToCheckout = this.driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a")); // Find Proceed to checkout 'a' tag
+		WebElement proceedToCheckout = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a"))); // Find Proceed to checkout 'a' tag
 		proceedToCheckout.click();
 		
 		// Checkout
 		
-		WebElement checkout = this.driver.findElement(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium")); // Find Proceed to checkout button
+		WebElement checkout = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium"))); // Find Proceed to checkout button
 		checkout.click();
 		
-		WebElement checkout2 = this.driver.findElement(By.cssSelector("#center_column > form > p > button")); // Find Proceed to checkout button
+		WebElement checkout2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > form > p > button"))); // Find Proceed to checkout button
 		checkout2.click();
 		
-		WebElement termsOfService = this.driver.findElement(By.cssSelector("#uniform-cgv")); // Find Terms of service checkbox
+		WebElement termsOfService = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#uniform-cgv"))); // Find Terms of service checkbox
 		termsOfService.click();
 		
-		WebElement checkout3 = this.driver.findElement(By.cssSelector("#form > p > button")); // Find Proceed to checkout button
+		WebElement checkout3 = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#form > p > button"))); // Find Proceed to checkout button
 		checkout3.click();
 		
-		WebElement payByBank = this.driver.findElement(By.cssSelector("#HOOK_PAYMENT > div:nth-child(1) > div > p > a")); // Find Pay by bank wire
+		WebElement payByBank = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#HOOK_PAYMENT > div:nth-child(1) > div > p > a"))); // Find Pay by bank wire
 		payByBank.click();
 		
-		WebElement confirm = this.driver.findElement(By.cssSelector("#cart_navigation > button")); // Find Confirm my order button
+		WebElement confirm = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#cart_navigation > button"))); // Find Confirm my order button
 		confirm.click();
 		
 		// Receipt
-		WebElement receipt = (WebElement) wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > div > p > strong"))); // Select receipt title
+		WebElement receipt = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#center_column > div > p > strong"))); // Select receipt title
 		
 		assertEquals("Your order on My Store is complete.", receipt.getText());
 		
